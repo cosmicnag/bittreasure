5f6c8893a308f6ea983726b7949ca54f87899e53
Yudhajit Nag <cosmicnag@gmail.com>
2014-10-25 21:14:02 +0530

    Magic byte fukup

--- hunts/utils.py
+++ hunts/utils.py
@@ -6,6 +6,7 @@ def getbountydetails (treasurehunt):
     # 3, Return object with all the above data
     m = treasurehunt.m
     n = treasurehunt.location_set.count()
+    magicbyte = 111 if treasurehunt.istestnet else 0
     privkeys = []
     pubkeys = []
     for i in range(0,n):
@@ -13,11 +14,10 @@ def getbountydetails (treasurehunt):
         privkeys.append(k)
         pubkeys.append(privtopub(k))
     script = mk_multisig_script (pubkeys,m,n)
-    bountyaddress = scriptaddr(script)
+    bountyaddress = scriptaddr(script, magicbyte=magicbyte)
     treasurehunt.address = bountyaddress
     locations = treasurehunt.location_set.all()
     for i,location in enumerate(locations):
         location.payload = privkeys[i]
         location.save()
     return treasurehunt
-    
